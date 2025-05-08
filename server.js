60a47af0eacd1169c7fa6f05209d597f0c787fc7
Sapinux <sapinux@gmail.com>
2025-05-08 18:40:32 -0300

    ajustes no server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var dgram = require("dgram");
 var server = dgram.createSocket("udp4");    //o tipo que o game maker suporta
 
 server.on("message", function (msg, rinfo) {
-    console.log(msg);   //mensagem do log
+    console.log(String(msg));   //mensagem do log
 });
 
 server.bind(62832)      //porta
