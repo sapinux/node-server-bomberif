4fb2a3a583066cee4b15b88ad9c3a6018f852285
Sapinux <sapinux@gmail.com>
2025-05-08 18:25:51 -0300

    criacao da funcao par receber buffer

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
 var dgram = require("dgram");
+
+var server = dgram.createSocket("udp4");    //o tipo que o game maker suporta
+
+server.on("message", function (msg, rinfo) {
+    console.log(msg);   //mensagem do log
+});
+
+server.bind(62832)      //porta
