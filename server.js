bdb6aa612184856d4aaeba4093f371640817a26e
Sapinux <sapinux@gmail.com>
2025-05-09 20:08:04 -0300

    adicao de msg de morte do player

--- server.js
+++ server.js
@@ -6,11 +6,12 @@ var data;
 
 server.on("message", function (msg, rinfo) {
     data = JSON.parse(msg);
-    if ("id" in data) console.log("id: " + String(data.id));
-    if ("x" in data) console.log("x: " + String(data.x));
-    if ("y" in data) console.log("y: " + String(data.y));
-    if ("b" in data) console.log("m: " + String(data.b));
-    if ("bp" in data) console.log("m: " + String(data.bp));
+    if ("id" in data) console.log("id: " + String(data.id));    //id do player
+    if ("x" in data) console.log("x: " + String(data.x));       //x do player
+    if ("y" in data) console.log("y: " + String(data.y));       //y do player
+    if ("b" in data) console.log("m: " + String(data.b));       //quando o player soltar bomba
+    if ("bp" in data) console.log("m: " + String(data.bp));     //quando o player pegar uma bomba com luva
+    if ("m" in data) console.log("m: " + String(data.m));       //quando o player morrer
     //console.log(String(msg));   //exibir a mensagem recebida no log
     //server.send("Recebido", rinfo.port, rinfo.address);
 });
