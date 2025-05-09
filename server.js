5be63c9095794170a98f3ea4f41b520616033dc2
Sapinux <sapinux@gmail.com>
2025-05-09 12:20:46 -0300

    ajustes

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var server = dgram.createSocket("udp4");    //"udp4": o tipo que o game maker su
 
 server.on("message", function (msg, rinfo) {
     console.log(String(msg));   //exibir a mensagem recebida no log
+    //server.send("Recebido", rinfo.port, rinfo.address);
 });
 
 server.bind(62832)      //porta
