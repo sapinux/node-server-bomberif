521f534343bb184260e4acbeb4e852ae17a4e12a
sapinux <sapinux@gmail.com>
2025-05-12 15:22:30 -0300

    add msg_type

--- server.js
+++ server.js
@@ -4,18 +4,39 @@ var server = dgram.createSocket("udp4");    //"udp4": o tipo que o game maker su
 
 var data;
 
+const msg_type = {
+    CREATE_HOST : 0,
+	JOIN_HOST : 1,
+	STOP_HOST : 2,
+    SET_PLAYER_STAT : 3
+}
+
 server.on("message", function (msg, rinfo) {
     data = JSON.parse(msg);
-    if ("id" in data) console.log("id: " + String(data.id));    //id do player
-    if ("x" in data) console.log("x: " + String(data.x));       //x do player
-    if ("y" in data) console.log("y: " + String(data.y));       //y do player
-    if ("b" in data) console.log("m: " + String(data.b));       //quando o player soltar bomba
-    if ("bp" in data) console.log("m: " + String(data.bp));     //quando o player pegar uma bomba com luva
-    if ("m" in data) console.log("m: " + String(data.m));       //quando o player morrer
+    console.log(String(msg));   //depuracao -----apagar depois!!!!!
+
+    if ("id" in data) console.log("< id: " + String(data.id));    //id do player
+    if ("x" in data) console.log("< x: " + String(data.x));       //x do player
+    if ("y" in data) console.log("< y: " + String(data.y));       //y do player
+    if ("b" in data) console.log("< m: " + String(data.b));       //quando o player soltar bomba
+    if ("bp" in data) console.log("< m: " + String(data.bp));     //quando o player pegar uma bomba com luva
+    if ("m" in data) console.log("< m: " + String(data.m));       //quando o player morrer
+    
+    switch (data.t) {
+        case msg_type.SET_PLAYER_STAT:
+            set_player_stat(data, rinfo);    
+            break
+    }
+    
+    //if ("t" in data) console.log("< m: " + String(data.t));       //maquina de estado
     //console.log(String(msg));   //exibir a mensagem recebida no log
     //server.send("Recebido", rinfo.port, rinfo.address);
     server.send(JSON.stringify(data), rinfo.port, rinfo.address); //envio de msg para o cliente
     //if (data.b) server.send(JSON.stringify(data.b), rinfo.port, rinfo.address); //envio de msg para o cliente
 });
+
+function set_player_stat(data, rinfo) {
+    console.log("Estamos no estado set player stat")
+}
 
 server.bind(62832)      //porta
